d429ff1fbe713c9e594b8403609c3244994976bc
brenw0rth <brenworth0@gmail.com>
2021-11-20 22:39:10 +0000

    www.py init

--- examples/www.py
+++ examples/www.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+'''
+A stupid server that hands out 200 OK responses.
+'''
 
-raise NotImplementedError
+import pync
+
+
+def main():
+    with pync.listen(args.host, args.port, k=True) as server:
+        for nc in server:
+            response = pync.makefile('HTTP/1.1 200 OK\r\n')
+            nc.readwrite(stdin=response)
+
+
+if __name__ == '__main__':
+    main()
 
