20ce97883b10d29c7a5d6ebe190d0b6cbe537d3e
brenw0rth <brenworth0@gmail.com>
2021-11-21 15:35:43 +0000

    www.py update

--- examples/www.py
+++ examples/www.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 '''
-A stupid server that hands out 200 OK responses.
+A stupid server that hands out HTTP 200 OK responses.
 '''
 
 import pync
 
 
 def main():
-    with pync.listen(args.host, args.port, k=True) as server:
-        for nc in server:
+    '''
+    This script demonstrates the use of the "k" option to keep
+    the server open after each connection.
+    '''
+
+    with pync.Netcat(8000, dest='localhost', l=True, k=True) as nc:
+        for connection in nc:
             response = pync.makefile('HTTP/1.1 200 OK\r\n')
-            nc.readwrite(stdin=response)
+            connection.readwrite(stdin=response)
 
 
 if __name__ == '__main__':
