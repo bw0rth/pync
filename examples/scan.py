cba2c56dc96633df2121e714baa18552e3475c1c
brenw0rth <brenworth0@gmail.com>
2021-11-21 22:34:42 +0000

    made a start on scan.py

--- examples/scan.py
+++ examples/scan.py
@@ -1,12 +1,34 @@
 # -*- coding: utf-8 -*-
+'''
+Example of a simple port scan using pync.
+'''
+
+import argparse
 
 import pync
 
 
 def main():
-    with pync.Netcat(args.host, args.port) as nc:
-        for connection in nc:
-            pass
+    parser = argparse.ArgumentParser('scan.py',
+            formatter_class=argparse.RawTextHelpFormatter,
+            description=__doc__,
+    )
+    parser.add_argument('dest',
+            help='Destination hostname or ip to connect to',
+            metavar='DEST',
+    )
+    parser.add_argument('port',
+            help='Port list or range for scanning',
+            metavar='PORT',
+            nargs='+',
+    )
+    args = parser.parse_args()
+
+    #print(args.port)
+    #return
+
+    with pync.Netcat(args.port, dest='localhost', v=True, z=True) as nc:
+        nc.run()
 
 
 if __name__ == '__main__':
