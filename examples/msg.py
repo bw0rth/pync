186b52219ac8cebe8ceb0be4867ea0dbc7fefa69
brenw0rth <brenworth0@gmail.com>
2021-11-21 16:36:34 +0000

    rename www.py to msg.py; added argparsing to allow custom message to send to clients.

---
+++ examples/msg.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+'''
+A stupid server that stays open and prints a message
+to each client that connects.
+'''
+
+import argparse
+
+import pync
+
+
+def main():
+    parser = argparse.ArgumentParser('www.py',
+            formatter_class=argparse.RawTextHelpFormatter,
+            description=__doc__,
+    )
+    parser.add_argument('dest',
+            help='Interface to bind the server to',
+            nargs='?',
+            metavar='DEST',
+            default='',
+    )
+    parser.add_argument('port',
+            help='Port number to bind the server to',
+            type=int,
+            metavar='PORT',
+    )
+    parser.add_argument('-m',
+            help='The message to send to clients',
+            metavar='MSG',
+            default='HTTP/1.1 200 OK',
+    )
+    args = parser.parse_args()
+
+    # The l option is for listen mode and k for keeping the server open
+    # between each client connection.
+    with pync.Netcat(args.port, dest=args.dest, l=True, k=True) as nc:
+        for conn in nc:
+            response = pync.makefile("{}\r\n".format(args.m))
+            conn.readwrite(stdin=response)
+
+
+if __name__ == '__main__':
+    main()
+
