a6d097ab9f073c17b2af16e7b4ce5ab4343a8f8d
brenw0rth <brenworth0@gmail.com>
2021-11-21 13:46:57 +0000

    scan.py init

--- examples/scan.py
+++ examples/scan.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
 
-raise NotImplementedError
+import pync
+
+
+def main():
+    with pync.Netcat(args.host, args.port) as nc:
+        for connection in nc:
+            pass
+
+
+if __name__ == '__main__':
+    main()
 
