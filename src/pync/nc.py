f21b0279b68657a2a91cbaed7ad2c37bd5369f67
brenw0rth <brenworth0@gmail.com>
2022-02-24 11:15:31 +0000

    -

--- src/pync/nc.py
+++ src/pync/nc.py
@@ -725,10 +725,8 @@ def pync(args, stderr=sys.stderr, **kwargs):
     :param kwargs: Any other key word arguments get passed to the
         underlying Netcat class.
 
-    Examples
-    ========
-
     .. code-block:: python
+       :caption: Examples
        
        from pync import pync
        pync('-l localhost 8000')
