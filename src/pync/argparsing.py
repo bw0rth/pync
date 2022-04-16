e6cdcbe1239eedb35972d7cc29a0ba2e4996ff90
brenw0rth <brenworth0@gmail.com>
2022-04-16 19:07:42 +0000

    refactor argparser to raise SystemExit on error

--- src/pync/argparsing.py
+++ src/pync/argparsing.py
@@ -2,30 +2,25 @@
 
 import argparse
 from collections import defaultdict
+import sys
 
 
-class GroupingArgumentParser(object):
+class GroupingArgumentParser(argparse.ArgumentParser):
 
     def __init__(self, *args, **kwargs):
-        self._parser = argparse.ArgumentParser(
-                *args,
-                **kwargs,
-        )
+        super(GroupingArgumentParser, self).__init__(*args, **kwargs)
         self._group_args = defaultdict(list)
         self._group_parsers = dict()
 
-    def __getattr__(self, name):
-        return getattr(self._parser, name)
-
     def add_argument(self, name, group='general arguments', **kwargs):
         if group not in self._group_parsers:
-            self._group_parsers[group] = self._parser.add_argument_group(group)
+            self._group_parsers[group] = super(GroupingArgumentParser, self).add_argument_group(group)
         parser = self._group_parsers[group]
         parser.add_argument(name, **kwargs)
         self._group_args[group].append(name.lstrip('-'))
 
     def parse_args(self, argv):
-        args = self._parser.parse_args(argv)
+        args = super(GroupingArgumentParser, self).parse_args(argv)
         arg_groups = defaultdict(argparse.Namespace)
         for group_name, arg_names in self._group_args.items():
             for arg_name in arg_names:
@@ -35,6 +30,13 @@ class GroupingArgumentParser(object):
                     continue
                 setattr(arg_groups[group_name], arg_name, arg_attr)
         return arg_groups
+
+    def error(self, message):
+        self.print_usage(sys.stderr)
+        args = {'prog': self.prog, 'message': message}
+        sys.stderr.write('%(prog)s: error: %(message)s\n' % args)
+        sys.stderr.flush()
+        raise SystemExit
 
 
 if __name__ == '__main__':
