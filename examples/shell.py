4e227379ffae37e7f817419f3d6047e4b6be4d5d
brenw0rth <brenworth0@gmail.com>
2022-04-16 10:02:39 +0000

    update examples

--- examples/shell.py
+++ examples/shell.py
@@ -16,6 +16,14 @@ import platform
 from pync import Netcat
 
 
+try:
+    # py2
+    input = raw_input
+except NameError:
+    # py3
+    pass
+
+
 def main():
     parser = argparse.ArgumentParser('shell.py',
             formatter_class=argparse.RawTextHelpFormatter,
@@ -23,13 +31,13 @@ def main():
     )
     parser.add_argument('dest',
             help='Hostname or IP to connect or bind to',
-            metavar='DEST',
+            metavar='dest',
             nargs='?',
             default='',
     )
     parser.add_argument('port',
             help='Port number to connect to',
-            metavar='PORT',
+            metavar='port',
             type=int,
     )
     parser.add_argument('-l',
@@ -37,6 +45,32 @@ def main():
             action='store_true',
     )
     args = parser.parse_args()
+
+    def print_warning(lines, ask_continue=False):
+        print('WARNING')
+        for l in lines:
+            print(l)
+        if ask_continue:
+            print()
+            response = ''
+            while response != 'Y':
+                response = input('Are you sure you want to continue? (Y/n): ')
+                if response == 'n':
+                    raise SystemExit
+
+    warning = ''
+    local_addresses = ('localhost', '127.0.0.1')
+    if args.l and args.dest not in local_addresses:
+        warning = 'You are about to bind a system shell to a non-local interface.'
+    elif args.dest and args.dest not in local_addresses:
+        warning = 'You are about to connect a system shell to a remote machine.'
+
+    if warning:
+        lines = (
+                warning,
+                'This may expose your system to attackers or eavesdroppers.',
+        )
+        print_warning(lines, ask_continue=True)
 
     command = "PS1='$ ' sh -i"
     if platform.system() == 'Windows':
@@ -49,8 +83,12 @@ def main():
             l=args.l,        # Listen for connections (server mode).
             v=True,          # Print connection status messages to stderr.
     )
-    with contextlib.closing(nc):
-        nc.readwrite()
+
+    try:
+        with contextlib.closing(nc):
+            nc.readwrite()
+    except KeyboardInterrupt:
+        print()
 
 
 if __name__ == '__main__':
