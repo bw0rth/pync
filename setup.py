ddfedfad1f8378033cdcd2ea2f8c26a23bd942e6
Brendon Worthington <brenworth0@gmail.com>
2022-04-17 22:31:41 +0100

    Update setup.py
    Change required Python version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description='arbitrary TCP and UDP connections and listens (Netcat for Python).',
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
-    python_requires=">=2.7",
+    python_requires=">=3.3",
     entry_points={
         'console_scripts': ['pync=pync.__main__:main'],
     },
