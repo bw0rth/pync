79685f64f1da440f1e9a6f07857b8e65216b211e
brenw0rth <brenworth0@gmail.com>
2022-08-26 13:10:04 +0000

    update chat.py example

--- examples/chat.py
+++ examples/chat.py
@@ -5,28 +5,89 @@ import sys
 from pync import pync, ConsoleInput
 
 
+try:
+    # py2
+    input = raw_input
+except NameError:
+    # py3
+    pass
+
+
 class ChatInput(ConsoleInput):
 
     def __init__(self, username, *args, **kwargs):
         super(ChatInput, self).__init__(*args, **kwargs)
         self.username = username
+        self.prompt = 'You: '
+        self._lines = [
+                "Chat started with user [{}]".format(self.username).encode(),
+        ]
 
     def readline(self):
+        if self._lines:
+            line = self._lines.pop(0)
+            return line+b'\n'
         line = super(ChatInput, self).readline()
         if line:
-            sys.stdout.buffer.write(self.username + b': ')
+            sys.stdout.write(self.prompt)
             sys.stdout.flush()
-            return self.username + b': ' + line
+            if line.strip():
+                return self.username.encode()+b': ' + line
+
+
+class ChatOutput(object):
+
+    def __init__(self):
+        self.prompt = 'You: '
+        self._newline = False
+
+    def write(self, data):
+        if not self._newline:
+            data = data.decode()
+            self._newline = True
+        else:
+            data = '\n'+data.decode()
+        sys.__stdout__.write(data)
+        sys.__stdout__.write(self.prompt)
+
+    def _write(self, data):
+        sys.__stdout__.write('\n\n'+data.decode())
+        sys.__stdout__.write(self.prompt)
+
+    def flush(self):
+        sys.__stdout__.flush()
 
 
 def main():
-    # Still Under Construction.
-    raise NotImplementedError
-
     parser = argparse.ArgumentParser('chat.py')
+    parser.add_argument('-l',
+            help='listen for a connection.',
+            action='store_true',
+    )
+    parser.add_argument('dest',
+            help='destination hostname or ip to connect or bind to.',
+    )
+    parser.add_argument('port',
+            help='port number to connect or bind to.',
+    )
+    parser.add_argument('--username',
+            help='username to use for the chat.',
+    )
+    args = parser.parse_args()
+    pync_args = '-v {dest} {port}'.format(
+            dest=args.dest,
+            port=args.port,
+    )
+    if args.l:
+        pync_args = '-l ' + pync_args
 
-    chatin = ChatInput(b'bren')
-    pync('localhost 8000', stdin=chatin)
+    username = args.username
+    while not username:
+        username = input('Enter username: ')
+
+    chatin = ChatInput(username)
+    chatout = ChatOutput()
+    return pync(pync_args, stdin=chatin, stdout=chatout)
 
 
 if __name__ == '__main__':
